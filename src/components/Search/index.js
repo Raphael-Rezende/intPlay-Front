ad766a5dc643cc209e3f2e718d99f493b6b76ff4
Pedro Rezende <pedro_rezendep3@hotmail.com>
2022-01-08 19:12:00 -0300

    part1 search

---
+++ src/components/Search/index.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useContext } from "react"
+import "./search.css"
+import SearchContext from "./context"
+
+const Search = ({ movies, type }) => {
+
+    const { updateSearchInput } = useContext(SearchContext)
+
+    useEffect(() => {
+
+
+    })
+
+
+    const renderPosters = (data) => {
+
+        return data.map((item, index) => {
+
+            if (item.capa) return <div key={index}><a href={`/details/${type}/${item._id}`} ><img src={`${process.env.REACT_APP_PUBLIC_URL}${item.capa}`} alt={"poster"} /></a></div>
+            return null
+
+
+        })
+    }
+
+
+    return (
+        <div className="search-background">
+
+            {movies ? (
+                <React.Fragment>
+                    {movies.length ? (
+                        <React.Fragment>
+                            <div className="search-container">{renderPosters(movies)}
+                                <button className="featured--close"
+                                    onClick={() => {
+                                        console.log('clicou limpar')
+                                        return updateSearchInput('')
+                                    }}
+                                >
+                                    <img
+                                        style={{ width: "30px" }}
+                                        src={"./close.png"}
+                                        alt="close icon"
+                                        onClick={() => console.log('clicou limpar')}
+                                    />
+                                </button>
+                            </div>
+                        </React.Fragment>
+
+                    ) : (<div className="not-found">Não encontramos nanda, desculpa mesmo! :/ </div>)}
+                </React.Fragment>
+            ) : (
+                <div className="loading-content">
+                    <div className="loading-circle"></div>
+                    <span className="loading-name">CARREGANDO...</span>
+                </div>
+
+            )}
+
+        </div>
+    )
+}
+
+export default Search
